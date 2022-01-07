030fc5e4e27b9714bc216a4daec7763276161a1c
NotJohnnySins <96237656+NotJohnnySins@users.noreply.github.com>
2022-01-07 18:39:41 +0200

    Update reversor.py

--- reversor.py
+++ reversor.py
@@ -1,3 +1,3 @@
-while True:
-    txt = input('Type a sentence to reverse ') 
-    print(txt[::-1])
+while True: # While this is true which basically while True is always true so FOR EVER
+    txt = input('Type a sentence to reverse ')  # Ask the user to put a sentence
+    print(txt[::-1]) # Print the string reversed
